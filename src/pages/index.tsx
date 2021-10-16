eb4f5be217efcd6c9a5fed8e8f4b0a596fa73adf
Alex L <38541529+alxn3@users.noreply.github.com>
2021-10-16 17:29:53 -0400

    Add responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <BaseLayout>
       <div className="flex items-center justify-center h-full flex-col py-10">
-        <div className="flex flex-1 justify-center items-center space-x-10 w-full pb-40">
+        <div className="flex flex-col md:flex-row flex-1 justify-center items-center space-x-10 w-full pb-40">
           <div>
             <h1 className="text-3xl sm:text-4xl font-semibold">
               Hello! I&apos;m Alex Lin
@@ -19,13 +19,13 @@ const Home = () => {
             <div className="p-4">
               <p className="text-2xl sm:text-3xl font-light">I&apos;m a...</p>
               <ShiftingText
-                className="text-5xl font-black ml-8"
+                className="text-5xl font-black ml-0 md:ml-8"
                 words={['Programmer', 'UX Designer', 'Engineer', 'Student']}
                 colors={['text-red-500', 'text-purple-500', 'text-yellow-500', 'text-pink-600']}
               />
             </div>
           </div>
-          <div className="w-1/4 lg:w-1/6">
+          <div className="w-1/3 md:w-1/4 mt-8 md:mt-0">
             <Image
               src={profilePic}
               width="1"
