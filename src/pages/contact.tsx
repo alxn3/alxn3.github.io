a29b4d93c02c875bee1c8500c31095583cb88798
Alex L <38541529+alxn3@users.noreply.github.com>
2021-10-18 17:46:31 -0400

    Add temporary contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,18 @@
+import React from 'react';
+import Footer from 'src/components/footer';
 import BaseLayout from 'src/layouts/base-layout';
 
 const Contact = () => {
   return (
     <BaseLayout>
-      <h1>Contact me!</h1>
+      <div className="flex justify-center h-full flex-col">
+        <div className="flex flex-1 items-center justify-center">
+          <h1 className="text-7xl font-black text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-red-600">
+            Under Construction...
+          </h1>
+        </div>
+        <Footer />
+      </div>
     </BaseLayout>
   );
 };
