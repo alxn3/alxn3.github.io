12fc4e978712b0604faba269125aed490f4174cc
Alex L <38541529+alxn3@users.noreply.github.com>
2021-10-20 16:36:12 -0400

    Create custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+const generateSwatch = (name) =>
+  Object.fromEntries(
+    Array.from(
+      {
+        length: 9,
+      },
+      (_, i) => [(i + 1) * 100, `var(--color-${name}-${(i + 1) * 100})`]
+    )
+  );
+
 module.exports = {
   mode: 'jit',
   purge: [
@@ -7,7 +20,18 @@ module.exports = {
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      ...defaultTheme.colors,
+      gray: colors.gray,
+      black: '#0F0E0E',
+    },
+    extend: {
+      colors: {
+        primary: generateSwatch('primary'),
+        secondary: generateSwatch('secondary'),
+        accent: generateSwatch('accent'),
+      },
+    },
   },
   variants: {
     extend: {},
