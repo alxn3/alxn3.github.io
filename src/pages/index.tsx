749046b1c46b6cbd2acc43eb766d0c0a898b667d
Alex L <38541529+alxn3@users.noreply.github.com>
2021-10-20 02:23:57 -0400

    Make spacing dynamic and sizing more responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,16 @@ const Home = () => {
   return (
     <BaseLayout
       headerContent={
-        <div className="flex flex-col items-center justify-around h-full md:justify-center md:py-10">
-          <div className="flex flex-col-reverse items-center justify-center md:flex-row-reverse md:flex-1 md:space-x-10 md:pb-40">
-            <div className="w-[16rem] rounded-[min(20rem,50%)] md:w-[min(30%,16rem)] mt-8 md:mt-0 hover:rounded-[2rem] hover:scale-105 md:hover:scale-110 md:hover:rotate-3 transition-all duration-1000 overflow-hidden peer">
+        <div className="flex flex-col items-center justify-around h-full md:py-10">
+          <div className="flex flex-col-reverse items-center justify-center md:flex-row-reverse md:flex-1 md:space-x-10">
+            <div className="w-[16rem] rounded-[min(20rem,50%)] md:w-[min(30%,16rem)] xl:w-[18rem] mt-8 md:mt-0 hover:rounded-[2rem] hover:scale-105 md:hover:scale-110 md:hover:rotate-3 transition-all duration-1000 overflow-hidden peer">
               <img
                 src="/panda.jpg"
                 className="object-cover h-auto width-full"
                 alt="me"
               />
             </div>
-            <div className="text-3xl sm:text-4xl peer-hover:scale-[1.03] md:peer-hover:scale-105 md:peer-hover:-translate-x-10 transition-transform duration-1000">
+            <div className="text-3xl sm:text-4xl xl:text-5xl peer-hover:scale-[1.03] md:peer-hover:scale-105 md:peer-hover:-translate-x-10 transition-transform duration-1000">
               <h1 className="font-semibold ">
                 Hello! I&apos;m Alex Lin
                 <br />
@@ -25,7 +25,7 @@ const Home = () => {
               <div className="sm:p-6">
                 <p className="font-light">I&apos;m a...</p>
                 <ShiftingText
-                  className="ml-0 text-5xl font-black md:ml-8"
+                  className="ml-0 text-5xl font-black xl:text-6xl md:ml-8"
                   words={[
                     'Programmer',
                     'UX Designer',
@@ -42,7 +42,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-center space-y-4">
+          <div className="flex flex-col items-center justify-end mt-4 space-y-4 h-1/5">
             <p className="font-mono text-gray-500">
               Scroll down to learn more...
             </p>
